76f7c8ae3ef12e6a9024771857be8c60616731fd
Modeste EKAMBI <tech@internis-engineering.com>
2024-09-15 16:30:53 +0100

    Loader added

--- script.js
+++ script.js
@@ -3,27 +3,43 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show loading
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete(){
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // New quote
 function newQuote() {
+    loading();
     // Pick a random quote from apiQuote array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
     quoteText.textContent = quote.text;
     authorText.textContent = (!quote.author) ? "Unknown" : quote.author;
+
+    // Set quote, hide loader
+    complete();
 }
 
 // Get quotes from API
 async function getQuotes(){
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    // const apiUrl = 'https://zenquotes.io/api/random'; deactivate due to CORS issues. To be resolved
     try {
         const reponse = await fetch(apiUrl);
         apiQuotes = await reponse.json();
-
         newQuote();
-
     } catch (error) {
         // Catch Error here
         console.log(error);
@@ -32,6 +48,7 @@ async function getQuotes(){
 
 // On load 
 getQuotes();
+
 
 // Tweet a quote
 function tweetQuote() {
